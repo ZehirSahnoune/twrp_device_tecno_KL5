d93f5a2ccf5a77b9adce6212d3be24cc76373f7c
ZehirSahnoune <144786633+ZehirSahnoune@users.noreply.github.com>
2026-05-09 22:03:26 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,18 +20,20 @@ TARGET_BOARD_PLATFORM := mt6768
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
-# Dynamic Partitions
-BOARD_SUPER_PARTITION_SIZE := 8589934592
-BOARD_SUPER_PARTITION_GROUPS := tecno_dynamic_partitions
-BOARD_TECNO_DYNAMIC_PARTITIONS_SIZE := 8585740288
-BOARD_TECNO_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product tr_product tr_mi tr_theme tr_region tr_company tr_carrier
-
-# Kernel Base and Offsets
+# Kernel - تم إضافة تعريف الاسم هنا لحل الخطأ الأخير
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_RAMDISK_OFFSET := 0x07c08000
 BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_INCLUDE_RECOVERY_DTBO := true
+
+# Dynamic Partitions
+BOARD_SUPER_PARTITION_SIZE := 8589934592
+BOARD_SUPER_PARTITION_GROUPS := tecno_dynamic_partitions
+BOARD_TECNO_DYNAMIC_PARTITIONS_SIZE := 8585740288
+BOARD_TECNO_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product tr_product tr_mi tr_theme tr_region tr_company tr_carrier
 
 # Kernel size and Ramdisk size
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
